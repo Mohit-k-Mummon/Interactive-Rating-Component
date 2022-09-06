4c98cd8aba8b1818c2e2dc098941ddb953a14c83
Mohit Mummon <mohitkumar335i@gmail.com>
2022-09-06 15:35:21 -0700

    JavaScript for functionality

---
+++ app.js
@@ -0,0 +1,20 @@
+const mainContainer = document.querySelector('.main-container');
+const thanksContainer = document.querySelector('.thankyou-container');
+
+const submitBtn = document.querySelector('#submit');
+const rateAgainBtn = document.querySelector('#rate-again');
+
+const rating = document.getElementById('rating');
+const rates = document.querySelectorAll('.rating-btn');
+
+submitBtn.addEventListener('click', () => {
+	thanksContainer.classList.remove('hidden');
+	mainContainer.classList.add('hidden');
+});
+
+rates.forEach(rate => {
+	rate.addEventListener('click', () => {
+		rating.innerHTML = rate.innerHTML;
+		rates.style.backgroundColor = 'red';
+	});
+});
